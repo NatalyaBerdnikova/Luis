67cbcc17e2703e7ee5e0678942349f135992f38e
NatalyaBerdnikova <kitakava@mail.ru>
2020-11-12 19:40:15 +0300

    Add files

---
+++ main.js
@@ -0,0 +1,31 @@
+let mySwiper = new Swiper('.js-slider', {
+    loop: true,
+    pagination: {
+      el: '.slider__pagination',
+      clickable: true,
+    }
+})
+
+let upButton = document.querySelector('.button--up');
+
+if (upButton !== undefined) {
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 750) {
+      upButton.classList.add('button--up-visible');
+    } else {
+      upButton.classList.remove('button--up-visible');
+    }
+  });
+}
+
+let menuItems = document.querySelectorAll('.menu__item')
+
+if (menuItems !== undefined) {
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function() {
+      let menuItemActive = document.querySelector('.menu__item--active');
+      menuItemActive.classList.remove('menu__item--active');
+      menuItems[i].classList.add('menu__item--active');
+    })
+  }
+}
